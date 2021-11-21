6177f952fb73ffde02379d6ac1fb07c0bc4da203
Reid Richard <reidrichardwilliams@gmail.com>
2021-11-21 18:21:36 -0500

    added internet permissions

--- app/src/main/java/com/example/assignment_5/DisplayRestaurants.java
+++ app/src/main/java/com/example/assignment_5/DisplayRestaurants.java
@@ -19,7 +19,7 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
-
+ 
 public class DisplayRestaurants extends AppCompatActivity {
 
     private ListView mListView;
